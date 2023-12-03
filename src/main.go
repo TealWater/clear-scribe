2f0720024bd0ab6e0810a904df2fbea36fc23f75
Matthew Straughn <matthewe99@hotmail.com>
2023-12-03 09:39:01 -0500

    added db connection

--- src/main.go
+++ src/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+
 	controller "github.com/TealWater/clear-scribe/src/Controller"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	cors "github.com/rs/cors/wrapper/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("unable to laod environment variables")
+	}
+
+}
 
 func main() {
 	router := gin.Default()
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	mongoURI := "mongodb+srv://" + os.Getenv("MONGO_DB_USERNAME") + ":" + os.Getenv("MONGO_DB_PASSWORD") + "@cluster0.lx82yxi.mongodb.net/?retryWrites=true&w=majority"
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+
+	mongoClient, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		log.Println("unable to connect to DB")
+		log.Panic(err)
+	}
+
+	defer func() {
+		if err = mongoClient.Disconnect(context.TODO()); err != nil {
+			log.Println("error trying to disconnect from the DB")
+			log.Panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	if err := mongoClient.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+	log.Println("Pinged your deployment. You successfully connected to MongoDB!")
 
 	router.GET("/time", func(c *gin.Context) {
 		c.JSON(200, gin.H{
