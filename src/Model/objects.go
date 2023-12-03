3a76042b78854f6f9374e26b5064f0e618145ed2
Matthew Straughn <matthewe99@hotmail.com>
2023-12-03 11:01:46 -0500

    added db object

--- src/Model/objects.go
+++ src/Model/objects.go
@@ -1,6 +1,10 @@
 package model
 
-import "mime/multipart"
+import (
+	"mime/multipart"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type IncomingText struct {
 	Message string `json:"message"`
@@ -8,4 +12,11 @@ type IncomingText struct {
 
 type FileUpload struct {
 	File *multipart.FileHeader `form:"file" binding:"required"`
+}
+
+type EditedEssay struct {
+	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	CreatedAt  primitive.DateTime `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	MessageOld string             `json:"messageOld,omitempty" bson:"messageOld,omitempty"`
+	MessageNew string             `json:"messageNew,omitempty" bson:"messageNew,omitempty"`
 }
