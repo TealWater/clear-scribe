ef4d7ffb709dcb6d86911367747c328a2cd9642e
Matthew Straughn <matthewe99@hotmail.com>
2023-12-27 11:52:38 -0500

    implemented /history upload and /delete functionality from DB (#11)

--- src/Controller/dbController.go
+++ src/Controller/dbController.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"time"
 
 	model "github.com/TealWater/clear-scribe/src/Model"
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -39,14 +37,6 @@ func init() {
 		log.Panic(err)
 	}
 
-	//running this will close the database, thus throwing an error
-	// defer func() {
-	// 	if err = mongoClient.Disconnect(context.TODO()); err != nil {
-	// 		log.Println("error trying to disconnect from the DB")
-	// 		log.Panic(err)
-	// 	}
-	// }()
-
 	// Send a ping to confirm a successful connection
 	if err := mongoClient.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
 		panic(err)
@@ -62,32 +52,9 @@ func init() {
 	database = mongoClient.Database(dbName)
 	collection = database.Collection(colName)
 	fmt.Println("collection name: ", collection.Name())
-
-	/* Testing */
-	// insertResult, err := collection.InsertOne(context.Background(), bson.D{
-	// 	{Key: "createdAt", Value: time.Now()},
-	// 	{Key: "messageOld", Value: "Taking a stroll in the park"},
-	// 	{Key: "messageNew", Value: "Taking a walk in the park"},
-	// })
-	// if err != nil {
-	// 	log.Fatal("failed at line 75!", err)
-	// }
-	// fmt.Println(insertResult.InsertedID)
-
-	// tempEssay := model.EditedEssay{
-	// 	CreatedAt:  primitive.NewDateTimeFromTime(time.Now()),
-	// 	MessageOld: "This book is a very cathardic read",
-	// 	MessageNew: "This book is a very relaxing read",
-	// }
-	// insertResult, err := collection.InsertOne(context.TODO(), tempEssay)
-	// if err != nil {
-	// 	log.Fatal("failed at line 75!", err)
-	// }
-	// fmt.Printf("Inserted %v!", insertResult.InsertedID)
-
 }
 
-func InsertMessages(messageOld, messageNew string) {
+func insertMessages(messageOld, messageNew string) {
 	entry := model.EditedEssay{
 		CreatedAt:  primitive.NewDateTimeFromTime(time.Now()),
 		MessageOld: messageOld,
@@ -103,75 +70,49 @@ func InsertMessages(messageOld, messageNew string) {
 	log.Println("Insereted new row entry with the id of: ", inserted.InsertedID)
 }
 
-func deleteMessages(messageId string) {
+func deleteMessage(messageId string) error {
 	id, _ := primitive.ObjectIDFromHex(messageId)
 	filter := bson.M{"_id": id}
 	deleteCount, err := collection.DeleteOne(context.Background(), filter)
 	if err != nil {
 		log.Println("unable to delete record from the database")
-		log.Fatal(err)
+		return err
 	}
 
 	log.Println("Message with id ", messageId, " was deleted, with count of: ", deleteCount.DeletedCount)
+	return nil
 }
 
-func deleteAllMessages() {
+func deleteAllMessages() error {
 	deleteCount, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
 	if err != nil {
 		log.Println("Unable to delete all records")
-		log.Fatal(err)
+		return err
 	}
 
 	log.Println("we deleted ", deleteCount.DeletedCount, " records")
+	return nil
 }
 
-func getAllMessages() ([]primitive.M, []primitive.M) {
+func getAllMessages() []primitive.M {
 	cur, err := collection.Find(context.Background(), bson.D{{}})
 	if err != nil {
 		log.Println("Unable to retreive all of the records")
 		log.Fatalln(err)
 	}
 
-	var messagesOld []primitive.M
-	var messagesNew []primitive.M
-
+	var messages []primitive.M
 	defer cur.Close(context.Background())
-	for cur.Next(context.Background()) {
-		var messageOld bson.M
-		var messageNew bson.M
 
-		if err := cur.Decode(&messageOld); err != nil {
+	for cur.Next(context.Background()) {
+		var message bson.M
+		if err := cur.Decode(&message); err != nil {
 			log.Println("unable to decode old message")
 			log.Fatalln(err)
 		}
-		if err := cur.Decode(&messageNew); err != nil {
-			log.Println("unable to decode new message")
-			log.Fatalln(err)
-		}
-
-		messagesOld = append(messagesOld, messageOld)
-		messagesNew = append(messagesNew, messageNew)
+		messages = append(messages, message)
 	}
-
-	return messagesOld, messagesNew
-}
-
-func GetAllRecords(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
-	c.Request.Method = "POST"
-	allRecordsOld, allRecordsNew := getAllMessages()
-	c.JSON(http.StatusOK, allRecordsOld)
-	c.JSON(http.StatusOK, allRecordsNew)
-}
-
-func AddARecord(c *gin.Context, msgOld, msgNew string) {
-	var mmg model.EditedEssay
-
-	mmg.MessageOld = msgOld
-	mmg.MessageNew = msgNew
-
-	InsertMessages(mmg.MessageOld, mmg.MessageNew)
-
+	return messages
 }
 
 func CloseDB() {
